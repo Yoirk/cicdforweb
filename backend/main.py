4f2722c264e2292c2f8c3b2df20b404f3dd33394
Yoirk <118036935+Yoirk@users.noreply.github.com>
2026-01-20 02:48:54 +0700

    Test Ruff scan

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,10 @@ DB_PATH = "/data/app.db"
 # Đổi sang argon2
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
+def calculate():
+    x =   
+    return 1
+
 def get_db():
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
